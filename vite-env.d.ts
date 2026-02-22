dcc78a8c80da263889c7bae47ae13c30ca9c79f8
Alakhx-dev <aniranjan170@gmail.com>
2026-02-22 11:49:43 +0530

    initial upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_OPENROUTER_API_KEY: string;
+  readonly VITE_MOCK_MODE?: string;
 }
 
 interface ImportMeta {
